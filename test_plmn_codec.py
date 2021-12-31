eae027819d66991ae52be5afdba53bf1d34ac12e
Tony Smith <smittytone@icloud.com>
2021-12-31 11:08:05 +0000

    Alter script import

--- test_plmn_codec.py
+++ test_plmn_codec.py
@@ -31,7 +31,7 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 IMPORTS
 '''
 import unittest
-import plmn_codec
+from plmn_codec import plmn_encoder, plmn_decoder, decode_table
 
 '''
 TEST CASES
@@ -40,37 +40,37 @@ class CodecTests(unittest.TestCase):
 
     def test_plmn_encoder(self):
         # Basic cases
-        self.assertEqual(plmn_codec.plmn_encoder("310", "410"), "1300144080")
-        self.assertEqual(plmn_codec.plmn_encoder("310", "260"), "1300624080")
+        self.assertEqual(plmn_encoder("310", "410"), "1300144080")
+        self.assertEqual(plmn_encoder("310", "260"), "1300624080")
 
         # Over-long MCC, MNC
-        self.assertEqual(plmn_codec.plmn_encoder("3108", "410333"), "1300144080")
+        self.assertEqual(plmn_encoder("3108", "410333"), "1300144080")
 
         # Short MNC
-        self.assertEqual(plmn_codec.plmn_encoder("289", "88"), "82F9884080")
+        self.assertEqual(plmn_encoder("289", "88"), "82F9884080")
 
 
     def test_plmn_decoder(self):
         # Basic cases
-        self.assertEqual(plmn_codec.plmn_decoder("1300144080"), ("310", "410"))
-        self.assertEqual(plmn_codec.plmn_decoder("1300624080"), ("310", "260"))
+        self.assertEqual(plmn_decoder("1300144080"), ("310", "410"))
+        self.assertEqual(plmn_decoder("1300624080"), ("310", "260"))
 
         # Over-long PLMN
-        self.assertEqual(plmn_codec.plmn_decoder("1300624080FFFFFF"), ("310", "260"))
+        self.assertEqual(plmn_decoder("1300624080FFFFFF"), ("310", "260"))
 
         # Short MNC
-        self.assertEqual(plmn_codec.plmn_decoder("82F9884080"), ("289", "88"))
+        self.assertEqual(plmn_decoder("82F9884080"), ("289", "88"))
 
 
     def test_decode_table(self):
         # Basic cases
-        self.assertEqual(plmn_codec.decode_table("AT+CRSM=214,28512,0,0,10,13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
-        self.assertEqual(plmn_codec.decode_table("10,13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
-        self.assertEqual(plmn_codec.decode_table("13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
-        self.assertEqual(plmn_codec.decode_table("+CRSM: 144,0,13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
+        self.assertEqual(decode_table("AT+CRSM=214,28512,0,0,10,13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
+        self.assertEqual(decode_table("10,13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
+        self.assertEqual(decode_table("13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
+        self.assertEqual(decode_table("+CRSM: 144,0,13006240801300144080"), "1. MCC: 310 MNC: 260\n2. MCC: 310 MNC: 410\n")
 
         # Short table entry
-        self.assertEqual(plmn_codec.decode_table("82F988408"), "")
+        self.assertEqual(decode_table("82F988408"), "")
 
 
 '''
